da9e3eb78c28e2f4cf57b69bf124059332240035
gz <zarubin.grigory@gmail.com>
2024-04-25 00:23:52 +0200

    incorporating trigrams into the word build up fn

--- src/textgen/word_length.clj
+++ src/textgen/word_length.clj
@@ -3,7 +3,8 @@
 ;; source for txt literature: http://digital.library.upenn.edu/books/
 ;; code taken from https://www.youtube.com/watch?v=C-kF25fWTO8
 
-(def book (slurp "https://www.mirrorservice.org/sites/ftp.ibiblio.org/pub/docs/books/gutenberg/2/4/8/2488/2488-8.txt"))
+;(def book (slurp "https://www.mirrorservice.org/sites/ftp.ibiblio.org/pub/docs/books/gutenberg/2/4/8/2488/2488-8.txt"))
+(def book (slurp "https://www.gutenberg.org/cache/epub/164/pg164.txt"))
 
 (def words (re-seq #"[\w|']+" book))
 (def num-words (float (count words)))
